b046fe97b74c0209d380e26b9f5870e52ae73215
j.horne2796@gmail.com <j.horne2796@gmail.com>
2014-08-22 19:45:46 -0400

    added ai opponents on main page

--- js/thehorne.js
+++ js/thehorne.js
@@ -3,10 +3,22 @@ var canvas = document.getElementById("fcss-pong");
 var circleSpeed = 180;
 
 var ball = document.getElementById("pong-ball");
-ball.posX = 100;
+ball.posX = 500;
 ball.posY = 100;
 ball.posXVel = circleSpeed;
 ball.posYVel = -circleSpeed;
+
+var paddleSpeed = 190;
+
+var paddle1 = document.getElementById("pong-paddle-1");
+paddle1.posX = canvas.offsetLeft - 15;
+paddle1.posY = 100;
+paddle1.speed = paddleSpeed;
+
+var paddle2 = document.getElementById("pong-paddle-2");
+paddle2.posX = canvas.offsetWidth - canvas.offsetLeft;
+paddle2.posY = 100;
+paddle2.speed = paddleSpeed;
 
 // Timer Stuff
 var now = new Date().getTime();
@@ -21,13 +33,14 @@ var getPos = function(elem) {
 var updateBallPos = function() {
 	ball.style.left = ball.posX + "px";
 	ball.style.top = ball.posY + "px";
-}
 
-var drawCircle = function(ctx, radius, x, y) {
-	ctx.beginPath();
-	ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-	ctx.fillStyle = "black";
-	ctx.fill();
+	// Might as well update paddles positions here
+	paddle1.style.top = paddle1.posY + "px";
+	paddle1.style.left = paddle1.posX + "px";
+
+
+	paddle2.style.top = paddle2.posY + "px";
+	paddle2.style.left = paddle2.posX + "px";
 }
 
 var pxToNumber = function(x) {
@@ -52,22 +65,39 @@ var gameloop = function() {
 	ball.posX += ball.posXVel * dt;
 	ball.posY += ball.posYVel * dt;
 
+
+	if (ball.posXVel < 0 && ball.posX <= (canvas.offsetWidth / 2)) {
+		if (ball.posY+10 < paddle1.posY+10) {
+			paddle1.posY -= paddle1.speed * dt;
+		} else {
+			paddle1.posY += paddle1.speed * dt;
+		}
+	}
+
+	if (ball.posXVel > 0 && ball.posX >= ((canvas.offsetWidth / 2)) + 50) {
+		if (ball.posY+10 < paddle2.posY+10) {
+			paddle2.posY -= paddle2.speed * dt;
+		} else {
+			paddle2.posY += paddle2.speed * dt;
+		}
+	}
+
 	// Circle Physics
-	if (ball.posY <= -10) {
+	if (ball.posY <= -5) {
 		ball.posYVel = -ball.posYVel;
 		ball.posX = ball.oldPosX;
 		ball.posY = ball.oldPosY;
-	} else if (ball.posY >= canvas.offsetHeight-10) {
+	} else if (ball.posY >= canvas.offsetHeight-20) {
 		ball.posYVel = -ball.posYVel;
 		ball.posX = ball.oldPosX;
 		ball.posY = ball.oldPosY;
 	}
 
-	if (ball.posX <= -10) {
+	if (ball.posX <= 0) {
 		ball.posXVel = -ball.posXVel;
 		ball.posX = ball.oldPosX;
 		ball.posY = ball.oldPosY;
-	} else if (ball.posX >= canvas.offsetWidth-10) {
+	} else if (ball.posX >= canvas.offsetWidth-30) {
 		ball.posXVel = -ball.posXVel;
 		ball.posX = ball.oldPosX;
 		ball.posY = ball.oldPosY;
