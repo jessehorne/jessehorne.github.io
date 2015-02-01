bcbd4d4777d4926a91afc3781de6a8d8396060e5
Jesse Horne <j.horne2796@gmail.com>
2015-02-01 08:12:39 -0500

    added first piece to writings

--- js/thehorne.js
+++ js/thehorne.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-  $("#aboutme").on("click", function() {
-    $("#html-stuff").load("./pages/aboutme.html");
-  });
+	$("#aboutme").on("click", function() {
+		$("#html-stuff").load("./pages/aboutme.html");
+	});
 
-  $("#goals").on("click", function() {
-    $("#html-stuff").load("./pages/goals.html");
-  });
+	$("#goals").on("click", function() {
+		$("#html-stuff").load("./pages/goals.html");
+	});
 
-  $("#games").on("click", function() {
-    $("#html-stuff").load("./pages/games.html");
-  });
+	$("#games").on("click", function() {
+		$("#html-stuff").load("./pages/games.html");
+	});
 
-  $("#writings").on("click", function() {
-    $("#html-stuff").load("./pages/writings.html");
-  });
+	$("#writings").on("click", function() {
+		$("#html-stuff").load("./pages/writings.html");
+	});
 
-  $("#apps").on("click", function() {
-    $("#html-stuff").load("./pages/apps.html");
-  });
+	$("#apps").on("click", function() {
+		$("#html-stuff").load("./pages/apps.html");
+	});
 });
