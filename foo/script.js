ef3024585896c5e2d8f15497d4db27a4b1cc1fd4
Jesse Horne <j.horne2796@gmail.com>
2015-09-03 17:01:11 -0400

    added foo

---
+++ foo/script.js
@@ -0,0 +1,30 @@
+function solve(x) {
+  var start_time = new Date().getTime() / 1000;
+
+  var number = x;
+  var iterator = 3;
+
+  while (number % 2 == 0) {
+    number = number / 2;
+  }
+
+  while (iterator <= number) {
+    var x = number % iterator;
+    if (x == 0) {
+      number = number / iterator;
+    } else {
+      iterator += 2;
+    }
+  }
+
+  var end_time = new Date().getTime() / 1000;
+  return (end_time - start_time);
+}
+
+var total = 0
+
+for (var x = 1; x <= 10000; x++) {
+  total += solve(600851475143);
+}
+
+document.getElementById("solution").innerHTML = total;
