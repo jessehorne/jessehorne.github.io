ebc449d2263a4c326a1f05724bc3f77e5a190e18
Jesse Horne <j.horne2796@gmail.com>
2015-11-02 18:09:34 -0500

    updated josephloses, pretty colors

--- josephloses/script.js
+++ josephloses/script.js
@@ -22,4 +22,28 @@ function makeJosephLose() {
             buffer = "";
         }
     }
+
+    prettify();
+}
+
+function prettify() {
+    setTimeout(function() {
+        requestAnimationFrame(prettify);
+    }, 100);
+
+    var r = Math.floor(Math.random() * 255) / 2;
+    var g = Math.floor(Math.random() * 255) / 2;
+    var b = Math.floor(Math.random() * 255) / 2;
+
+    document.getElementById("wat2").style["color"] = "rgb(" + r + "," + g + "," + b + ")";
+
+    var r = Math.floor(Math.random() * 255) / 2;
+    var g = Math.floor(Math.random() * 255) / 2;
+    var b = Math.floor(Math.random() * 255) / 2;
+
+    document.getElementById("wat2").style["background-color"] = "rgb(" + r + "," + g + "," + b + ")";
+
+    var randomSize = 10 + Math.floor(Math.random() * 32);
+
+    document.getElementById("wat2").style["font-size"] = randomSize;
 }
